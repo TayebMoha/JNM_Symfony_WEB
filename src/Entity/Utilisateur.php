8aeb284361b550f82fc045ce850bf08c9266f31b
Tayeb <tayeb.mohammedi@kleegroup.com>
2022-12-23 18:27:37 +0100

    [login+registration] implementation securite

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -6,10 +6,12 @@ use App\Repository\UtilisateurRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -61,6 +63,9 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\ManyToMany(targetEntity: Statut::class, inversedBy: 'refUtilisateur')]
     private Collection $refStatut;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -290,6 +295,18 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function removeRefStatut(Statut $refStatut): self
     {
         $this->refStatut->removeElement($refStatut);
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
